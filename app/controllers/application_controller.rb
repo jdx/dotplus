1ffe7b53d02db0e8482cc469dc9db266f7459454
dickeyxxx <jeff@dickey.xxx>
2013-05-18 12:59:07 -0700

    time zone support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  around_filter :set_time_zone
 
   protected
 
@@ -17,4 +19,13 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by_id(session[:user_id])
   end
   helper_method :current_user
+
+  def current_city
+    @current_city ||= City.find_by_subdomain(request.subdomain)
+  end
+  helper_method :current_city
+
+  def set_time_zone(&block)
+    Time.use_zone(current_city.try(:time_zone) || "UTC", &block)
+  end
 end
